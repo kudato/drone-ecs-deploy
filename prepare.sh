cf029819549722dd25851b26f03bd75295399b23
Alexander Shevchenko <kudato@me.com>
2019-06-18 14:48:35 +0300

    Rename DEPLOY_COMMIT param to DEPLOY_TAG.

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -n "${PLUGIN_DEPLOY_COMMIT}" ]
+if [ -n "${PLUGIN_DEPLOY_TAG}" ]
+then
+    export DEPLOY_TAG=${PLUGIN_DEPLOY_TAG}
+fi
+
+if [ -n "${DEPLOY_TAG}" ]
 then
     if [ -n "${PLUGIN_COMPOSE_FILE}" ]
     then
@@ -8,5 +13,5 @@ then
     else
         export COMPOSE_FILE=docker-compose.yml
     fi
-    sed -i "s|{{tag}}|${PLUGIN_DEPLOY_COMMIT}|g" "${COMPOSE_FILE}"
+    sed -i "s|{{tag}}|${DEPLOY_TAG}|g" "${COMPOSE_FILE}"
 fi
